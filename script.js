6e9ed652f91b1e30fa473e7417c314ded4667cdf
Javier Vargas <vargassosa.javier@gmail.com>
2024-10-28 15:52:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Muestra o oculta información adicional
+function mostrarInfo(id) {
+  const element = document.getElementById(id);
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
